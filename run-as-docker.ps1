f55884412dc1e595618f6e2da62d36c43b4baa25
Aranga Nanayakkara <aranga-nana@users.noreply.github.com>
2017-04-17 14:18:21 +1000

    Update run-as-docker.ps1

--- run-as-docker.ps1
+++ run-as-docker.ps1
@@ -1,1 +1,1 @@
-docker run --name dev --link mysql -v c:\development\maytech_repo\ourceo\src:/var/www -p 8080:80 -d aranga/zend-dev
+docker run --name dev --link mysql -v c:\development\php-app\src:/var/www -p 8080:80 -d aranga/zend-dev
